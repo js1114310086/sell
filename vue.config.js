98810070a8b426106ce5f60999e6d10fb1eaee27
粉猪佩奇 <1114310086@qq.com>
2019-09-07 19:06:29 +0800

    veu

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,6 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     disableHostCheck: true,
-    host: '127.0.0.1',
-    port: 8080,
-    open: true
   }
   // devServer: { 
   //   // host: '0.0.0.0',
